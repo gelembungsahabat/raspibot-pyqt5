3f01a6cbec22b5ffaf6be6f2bb87a7d25bab5d8a
wildan <wildanrgsspeed@gmail.com>
2020-08-19 04:32:37 +0700

    initial commit

---
+++ UI_main.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'untitled.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+'''
+This code made by Muhammad Wildan
+release time: 19-08-2020, 04:07 WIB
+
+nb: ini program tampilan/UI yang digenerate dari QTdesigner
+
+'''
+
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(730, 473)
+        self.pushButton_4 = QtWidgets.QPushButton(Form)
+        self.pushButton_4.setGeometry(QtCore.QRect(550, 280, 91, 71))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(Form)
+        self.pushButton_5.setGeometry(QtCore.QRect(630, 200, 91, 71))
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(Form)
+        self.pushButton_6.setGeometry(QtCore.QRect(470, 200, 91, 71))
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.pushButton_7 = QtWidgets.QPushButton(Form)
+        self.pushButton_7.setGeometry(QtCore.QRect(550, 120, 91, 71))
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(10, 20, 411, 441))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+        # biar gabisa hover pake arrow key
+        self.pushButton_4.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.pushButton_5.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.pushButton_6.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.pushButton_7.setFocusPolicy(QtCore.Qt.NoFocus)
+
+        # shortcut button
+        QtWidgets.QShortcut(QtCore.Qt.Key_Up, self.pushButton_4,
+                            self.pushButton_7.animateClick)
+        QtWidgets.QShortcut(QtCore.Qt.Key_Right, self.pushButton_4,
+                            self.pushButton_5.animateClick)
+        QtWidgets.QShortcut(QtCore.Qt.Key_Down, self.pushButton_4,
+                            self.pushButton_4.animateClick)
+        QtWidgets.QShortcut(QtCore.Qt.Key_Left, self.pushButton_4,
+                            self.pushButton_6.animateClick)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton_4.setText(_translate("Form", "Mundur"))
+        self.pushButton_5.setText(_translate("Form", "Kanan"))
+        self.pushButton_6.setText(_translate("Form", "Kiri"))
+        self.pushButton_7.setText(_translate("Form", "Maju"))
+        self.label.setText(_translate("Form", "Mode Navigasi"))
---
+++ main.py
@@ -0,0 +1,110 @@
+'''
+This code made by Muhammad Wildan
+release time: 19-08-2020, 04:07 WIB
+
+nb: ini program logicnya.. kalo pengen ngirim data serial ke microcontroller, 
+    hilangkan semua yang mempunyai satu hashtag (#)
+
+'''
+
+##import librarynya
+import cv2
+import sys
+import imutils
+# import serial
+
+##import dependencynya
+from UI_main import *
+from PyQt5.Qt import Qt
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QImage
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QApplication
+
+## ini port serialnya, jika error, bisa di googling hehe
+# serialData = serial.Serial('/dev/ttyUSB0')
+
+
+##ini class main windownya
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+        ##set timer agar dapat mengakses camera tiap satuan waktu
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.viewCam)
+        self.controlTimer()
+
+        ##set/connect push buttonnya ke fungsi stop
+        self.ui.pushButton_4.clicked.connect(self.stop)
+        self.ui.pushButton_5.clicked.connect(self.stop)
+        self.ui.pushButton_6.clicked.connect(self.stop)
+        self.ui.pushButton_7.clicked.connect(self.stop)
+
+    ##fungsi untuk mengakses camera dengan opencv 
+    def viewCam(self):
+        ##ngeread capture image dari webcam/camera
+        ret, image = self.cap.read()
+        
+        ##format warnanya diconvert dari BGR ke RGB
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        ##resize gambar pake library `imutils` trus hasilnya dimasukkan kedalam QPixmap
+        img = imutils.resize(image, width=480)
+        height, width, channel = img.shape
+        step = channel * width
+        qImg = QImage(img.data, width, height, step, QImage.Format_RGB888)
+        self.ui.label.setPixmap(QPixmap.fromImage(qImg))
+
+    ##ini buat ngontrol timer buat ngambil gambar `frame by frame`
+    def controlTimer(self):
+        if not self.timer.isActive():
+            self.cap = cv2.VideoCapture(0)
+            self.timer.start(1)
+
+    ##tekan esc untuk exit dari GUI
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Escape:
+            self.close()
+
+
+    ##ini buat ngakses tombolnya pake keyboard
+    def keyReleaseEvent(self, event):
+        ##tombol arah atas
+        if event.key() == Qt.Key_Up:
+            print("Maju")
+            # data = str("w")
+            # serialData.write(data.encode())
+
+        ##tombol arah kanan
+        elif event.key() == Qt.Key_Right:
+            print("Kanan")
+            # data = str("d")
+            # serialData.write(data.encode())
+
+        ##tombol arah bawah
+        elif event.key() == Qt.Key_Down:
+            print("Mundur")
+            # data = str("s")
+            # serialData.write(data.encode())
+
+        ##tombol arah kiri
+        elif event.key() == Qt.Key_Left:
+            print("Kiri")
+            # data = str("a")
+            # serialData.write(data.encode())
+
+    #fungsi buat deteksi tombol keyboardnya telah dilepas (key_release)
+    def stop(self):
+        print("stop!")
+
+
+## ini program intinya
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
